5f6092734386a28fb489cdc38dd4d509b394d76a
vahan akopjanian <vahan.bclub@gmail.com>
2020-03-04 20:44:43 +0300

    Updates

---
+++ precache-manifest.3a3a3b64871e59922c231efb0ebf32c1.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa9be625241dd58d2337e5d4c2acad7",
+    "url": "/food_app_deploy/index.html"
+  },
+  {
+    "revision": "0a325b3a68e25fa65457",
+    "url": "/food_app_deploy/static/css/2.18e5b7a7.chunk.css"
+  },
+  {
+    "revision": "acf225cc18daa6d6b67b",
+    "url": "/food_app_deploy/static/css/main.92e774fc.chunk.css"
+  },
+  {
+    "revision": "0a325b3a68e25fa65457",
+    "url": "/food_app_deploy/static/js/2.b560f7a6.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/food_app_deploy/static/js/2.b560f7a6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "acf225cc18daa6d6b67b",
+    "url": "/food_app_deploy/static/js/main.b09b131b.chunk.js"
+  },
+  {
+    "revision": "325f1c7b8424447ff148",
+    "url": "/food_app_deploy/static/js/runtime-main.d437c434.js"
+  },
+  {
+    "revision": "f0bb9f93d20c5dc8f0bf5deeb48f710b",
+    "url": "/food_app_deploy/static/media/1.f0bb9f93.jpg"
+  },
+  {
+    "revision": "b7caa2556521d673aa01512dd412d95d",
+    "url": "/food_app_deploy/static/media/arrowToRight.b7caa255.svg"
+  },
+  {
+    "revision": "b7dbcaad461d6cc89e7460fab57c35b1",
+    "url": "/food_app_deploy/static/media/author.b7dbcaad.jpg"
+  },
+  {
+    "revision": "1b8fac283cd37f95de162f97ede842c4",
+    "url": "/food_app_deploy/static/media/bg_2.1b8fac28.jpg"
+  },
+  {
+    "revision": "6ba54e47453d45a2cf307f6c3568add7",
+    "url": "/food_app_deploy/static/media/bg_3.6ba54e47.jpg"
+  },
+  {
+    "revision": "43268711718843cd621f877d4e06927a",
+    "url": "/food_app_deploy/static/media/bg_4.43268711.jpg"
+  },
+  {
+    "revision": "16fd90be6705f6fa81d309fefe1e9e77",
+    "url": "/food_app_deploy/static/media/bg_5.16fd90be.jpg"
+  },
+  {
+    "revision": "d3078c7f91c2646f85ddf2ca9ecd38c7",
+    "url": "/food_app_deploy/static/media/email-envelope-button.d3078c7f.svg"
+  },
+  {
+    "revision": "a8f4bf4714dd41751fb294c6a418618f",
+    "url": "/food_app_deploy/static/media/facebook.a8f4bf47.svg"
+  },
+  {
+    "revision": "edbdd8cc3b9d4ccd1eaa74980a1f9f9d",
+    "url": "/food_app_deploy/static/media/instagram.edbdd8cc.svg"
+  },
+  {
+    "revision": "1d59e3c78fae275fc455afc5587ff6ea",
+    "url": "/food_app_deploy/static/media/magnifying-glass.1d59e3c7.svg"
+  },
+  {
+    "revision": "328e0f25800f9d33b604a3c9644b5f6e",
+    "url": "/food_app_deploy/static/media/pin.328e0f25.svg"
+  },
+  {
+    "revision": "2be1e204ebd146af9c0f79f2afc34ced",
+    "url": "/food_app_deploy/static/media/play-button-arrowhead.2be1e204.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/food_app_deploy/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/food_app_deploy/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/food_app_deploy/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/food_app_deploy/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b0324aad8658499adc70a94ab6d9de10",
+    "url": "/food_app_deploy/static/media/telegram.b0324aad.svg"
+  },
+  {
+    "revision": "bbe88ba75895bdd3c617bb4b90ad3acf",
+    "url": "/food_app_deploy/static/media/telephone-handle-silhouette.bbe88ba7.svg"
+  },
+  {
+    "revision": "5790426f54c17e801c37e7890d5d7cc7",
+    "url": "/food_app_deploy/static/media/twitter.5790426f.svg"
+  }
+]);
